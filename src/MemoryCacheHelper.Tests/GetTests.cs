82b441acaa98cf3ec61c30b06c1a9d65dc4b5e64
Hendy Racher <git@hendyracher.co.uk>
2018-07-29 21:00:56 +0100

    removed obsoleted methods

---
+++ src/MemoryCacheHelper.Tests/GetTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryCacheHelper.Tests
+{
+    [TestClass]
+    public class GetTests : BaseTests
+    {
+        [TestMethod]
+        public void Get_Unknown_Key()
+        {
+            var found = false;
+
+            var output = MemoryCache.Instance.Get<string>(KEY, out found);
+
+            Assert.IsFalse(found);
+
+            Assert.IsNull(output);
+        }
+
+        [TestMethod]
+        public void Get_String()
+        {
+            var input = "hello world";
+
+            MemoryCache.Instance.Set(KEY, input);
+
+            var output = MemoryCache.Instance.Get<string>(KEY);
+
+            Assert.AreEqual(input, output);
+        }
+    }
+}
--- src/MemoryCacheHelper/MemoryCache.cs
+++ src/MemoryCacheHelper/MemoryCache.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Caching;
 
 namespace MemoryCacheHelper
 {
@@ -32,74 +30,6 @@ namespace MemoryCacheHelper
         internal IEnumerable<string> GetOrderedKeys()
         {
             return this._memoryCache.Select(x => x.Key).OrderBy(x => x);
-        }
-
-        /// <summary>
-        /// Queries key in cache for object of type T, otherwise executes expensiveFunc and ensures cache is set
-        /// </summary>
-        /// <typeparam name="T">type of object expected</typeparam>
-        /// <param name="cacheKey">key to the cache item to get (or set if not found)</param>
-        /// <param name="expensiveFunc">function to execute if cache item not found</param>
-        /// <param name="timeout">(optional) number of seconds before cache value should time out, default 0 = no timeout</param>
-        /// <returns>an object from cache of type T, else the result of the expensiveFunc</returns>
-        [Obsolete("Use AddOrGetExisting<T>(string, Func<T>) instead")]
-        public T GetSet<T>(string cacheKey, Func<T> expensiveFunc, int timeout = 0)
-        {
-            bool found;
-            T cachedObject = this.Get<T>(cacheKey, out found);
-
-            if (!found)
-            {
-                this._cacheKeysBeingHandled.TryAdd(cacheKey, new CacheKeyBeingHandled());
-
-                lock (this._cacheKeysBeingHandled[cacheKey].Lock)
-                {
-                    // re-check to see if another thread beat us to setting this value
-                    cachedObject = this.Get<T>(cacheKey, out found);
-                    if (!found)
-                    {
-                        cachedObject = expensiveFunc();
-
-                        this.Set(cacheKey, cachedObject, timeout);
-                    }
-                }
-
-                CacheKeyBeingHandled cacheKeyBeingHandled;
-                this._cacheKeysBeingHandled.TryRemove(cacheKey, out cacheKeyBeingHandled);
-            }
-
-            return cachedObject;
-        }
-
-        /// <summary>
-        /// Populate the cache key with the supplied object - supplying null will remove cache item
-        /// </summary>
-        /// <param name="cacheKey">key for the cache item to set</param>
-        /// <param name="objectToCache">object to put into the cache item, null will remove cache item</param>
-        /// <param name="timeout">number of seconds before cache value should time out, default 0 = no timeout</param>
-        [Obsolete("Use Set(string, object) instead")]
-        public void Set(string cacheKey, object objectToCache, int timeout)
-        {
-            if (objectToCache != null)
-            {
-                // TODO: cancel any existing expensive function that may be setting this key (as we have object ready to cache here)
- 
-                if (timeout > 0)
-                {
-                    this._memoryCache.Set(
-                        cacheKey,
-                        objectToCache,
-                        new CacheItemPolicy() { AbsoluteExpiration = DateTime.Now.AddSeconds(timeout) });
-                }
-                else
-                {
-                    this._memoryCache[cacheKey] = objectToCache;
-                }
-            }
-            else
-            {
-                this.Remove(cacheKey);
-            }
         }
 
         /// <summary>
