028da77191869defa965c83fa5ba918e969401da
Hendy Racher <git@hendyracher.co.uk>
2018-08-02 00:43:35 +0100

    added wrapper method for Trim(int percent)

---
+++ src/MemoryCacheHelper/MemoryCache_Trim.cs
@@ -0,0 +1,15 @@
+﻿namespace MemoryCacheHelper
+{
+    public sealed partial class MemoryCache
+    {
+        /// <summary>
+        /// Removes a specified percentage of cache entries from the cache object
+        /// </summary>
+        /// <param name="percent">The percentage of total cache entries to remove</param>
+        /// <returns>The number of entries removed from the cache</returns>
+        public long Trim(int percent)
+        {
+            return this._memoryCache.Trim(percent);
+        }
+    }
+}
