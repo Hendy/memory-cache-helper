d3c446f0cbe7c4a7b74b195ca54a8f21eb462236
Hendy Racher <git@hendyracher.co.uk>
2018-08-02 23:24:09 +0100

    updated versioning to 0.6.0

--- src/MemoryCacheHelper.Tests/Properties/AssemblyInfo.cs
+++ src/MemoryCacheHelper.Tests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("d4eb7d33-014a-4744-bb6b-a7bcf4b3d688")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
