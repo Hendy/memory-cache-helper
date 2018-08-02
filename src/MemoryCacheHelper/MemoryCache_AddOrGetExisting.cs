261fb6265210006405fa1fdd539d94ff5e0531d6
Hendy Racher <git@hendyracher.co.uk>
2018-08-02 21:14:56 +0100

    Added GetSet<T>() as a wrapper to AddOrGetExisting() #2

--- src/MemoryCacheHelper/MemoryCache_AddOrGetExisting.cs
+++ src/MemoryCacheHelper/MemoryCache_AddOrGetExisting.cs
@@ -9,11 +9,11 @@ namespace MemoryCacheHelper
     public sealed partial class MemoryCache
     {
         /// <summary>
-        /// Gets or inserts a cache entry into the cache by using a key and a value and optional eviction
+        /// Gets or inserts a cache entry into the cache by using a key and a function and optional eviction
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="key">A unique identifier for the cache entry to insert</param>
-        /// <param name="valueFunction"></param>
+        /// <param name="valueFunction">A function to execute to get the value for the cache entry</param>
         /// <param name="policy">(Optional) An object that contains eviction details for the cache entry</param>
         /// <returns></returns>
         public T AddOrGetExisting<T>(string key, Func<T> valueFunction, CacheItemPolicy policy = null)
---
+++ src/MemoryCacheHelper/MemoryCache_GetSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace MemoryCacheHelper
+{
+    public sealed partial class MemoryCache
+    {
+        /// <summary>
+        /// Gets or inserts a cache entry into the cache by using a key and a function and optional eviction
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">A unique identifier for the cache entry to insert</param>
+        /// <param name="valueFunction">A function to execute to get the value for the cache entry</param>
+        /// <param name="policy">(Optional) An object that contains eviction details for the cache entry</param>
+        /// <returns></returns>
+        public T GetSet<T>(string key, Func<T> valueFunction, CacheItemPolicy policy = null)
+        {
+            return this.AddOrGetExisting(key, valueFunction, policy);
+        }
+    }
+}
